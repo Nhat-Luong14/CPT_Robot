55d64d9cc63b93dd1b2e70f2eeddbd6573f659e4
nhat.luongduc@gmail.com <nhat.luongduc@gmail.com>
2021-02-09 19:28:08 +0900

    first run on robot

--- gauss_process/re_form/config.py
+++ gauss_process/re_form/config.py
@@ -1,4 +1,4 @@
-server_ip       = "192.168.1.31"
+server_ip       = "192.168.2.214"
 server_port     = 10002
-data_len        = 700
+data_len        = 7000
 data_file_name  = "data/Study_gaussian_new_new.csv"
