ddf66a8583120009c97d7966f421cdc3d48ea1c3
Nhat Luong <nhat.luongduc@gmail.com>
2021-06-29 16:07:32 +0900

    added source probability map updating using infotaxis

---
+++ infotaxis/bombyx_2.0/config.py
@@ -0,0 +1,17 @@
+src_pos     = (0.0, 1.5)
+src_radius  = 0.3
+start_pos   = (4.0, 2.5)
+grid        = (21, 11)
+x_bounds    = [0, 4.0]
+y_bounds    = [0, 3.0]
+
+max_dur     = 120 #secound
+dt          = 1   #second
+wind        = 0.5 #m/s
+p_lifetime  = 1000 #second
+diffusivity = 0.05     #(m^2/s) ????????????????????
+step_size   = 0.5 #m
+
+searcher_size = 0.003 #m
+emission_rate = 5 #Hz
+seed = 8
