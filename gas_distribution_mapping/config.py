de5fb1b21ecf759dc192778465b8d658a64a1142
Nhat Luong <nhat.luongduc@gmail.com>
2021-07-05 19:24:40 +0900

    origin_config_file

--- gas_distribution_mapping/config.py
+++ gas_distribution_mapping/config.py
@@ -1,13 +1,6 @@
-# scale = 1000        #m to mm
-# x_dim = 2400        #mm
-# y_dim = 1700        #mm
-# sigma = 150         #mm
-# resolution = 50     #mm
-# cutoff_radius = 3*sigma
-
 scale = 1000        #m to mm
-x_dim = 200        #mm
-y_dim = 500        #mm
+x_dim = 2400        #mm
+y_dim = 1700        #mm
 sigma = 150         #mm
 resolution = 50     #mm
 cutoff_radius = 3*sigma
