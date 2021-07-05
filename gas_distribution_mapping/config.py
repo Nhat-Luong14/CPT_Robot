d60e67604051c48691e6045d8e9e550e0df6f321
Nhat Luong <nhat.luongduc@gmail.com>
2021-07-05 19:20:56 +0900

    add folder

--- gas_distribution_mapping/config.py
+++ gas_distribution_mapping/config.py
@@ -1,6 +1,13 @@
+# scale = 1000        #m to mm
+# x_dim = 2400        #mm
+# y_dim = 1700        #mm
+# sigma = 150         #mm
+# resolution = 50     #mm
+# cutoff_radius = 3*sigma
+
 scale = 1000        #m to mm
-x_dim = 2400        #mm
-y_dim = 1700        #mm
+x_dim = 200        #mm
+y_dim = 500        #mm
 sigma = 150         #mm
 resolution = 50     #mm
 cutoff_radius = 3*sigma
